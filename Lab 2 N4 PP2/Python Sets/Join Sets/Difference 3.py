45054d75cca090acfac12c128c5dde6f1ba1d4d9
qbylqi <aralbaiabylai@gmail.com>
2024-09-19 13:32:59 +0500

    Add files via upload

---
+++ Lab 2 N4 PP2/Python Sets/Join Sets/Difference 3.py
@@ -0,0 +1,6 @@
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.difference_update(set2)
+
+print(set1)
---
+++ Lab 2 N4 PP2/Python Sets/Join Sets/Symmetric Differences 3.py
@@ -0,0 +1,6 @@
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.symmetric_difference_update(set2)
+
+print(set1)
