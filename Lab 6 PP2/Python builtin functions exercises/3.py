e7912628a1b5f9327bb971d039dc3f9a71de157d
qbylqi <aralbaiabylai@gmail.com>
2024-10-17 13:10:55 +0500

    3.py

--- Lab 6 PP2/Python builtin functions exercises/3.py
+++ Lab 6 PP2/Python builtin functions exercises/3.py
@@ -1,8 +1,8 @@
 # Определяем функцию для проверки на палиндром
+s = input()
 def is_palindrome(s):
     return s == s[::-1]  # Сравниваем строку с её обратной версией
-
-# Пример
-s = "madam"
+    
+# Выводим результат
 result = is_palindrome(s)
-print(result)  # Выводим результат
+print(result)  
