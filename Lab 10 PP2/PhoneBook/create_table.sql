4df1c612a3894878e108695edb900afa91b33660
qbylqi <aralbaiabylai@gmail.com>
2024-11-30 17:03:44 +0500

    Delete Lab 10 PP2 directory

--- Lab 10 PP2/PhoneBook/create_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE phonebook (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(200) NOT NULL,
-    last_name VARCHAR(200) NOT NULL,
-    username VARCHAR(200) UNIQUE NOT NULL,
-    phone_number VARCHAR(200) NOT NULL
-);
